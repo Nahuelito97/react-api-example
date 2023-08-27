61541378a18a253bcc15e660fb2acceec25a6149
Nahuelito97 <nahuel.wagner97@gmail.com>
2023-08-26 21:47:52 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  baseUrl: 'https://Nahuelito97.github.io/react-api/',
+  plugins: [react()],
+})
